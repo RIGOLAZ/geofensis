51f807bd8e1c3660198e89519f95637a1e83e190
unknown <rigolaz@gmail.com>
2026-03-03 02:19:42 +0100

    Devices/create Ok

--- web-admin/src/App.jsx
+++ web-admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import Layout from './components/layout/Layout';
@@ -9,10 +9,10 @@ import ZonesListPage from './pages/ZonesPage/ZonesListPage/ZonesListPage';
 import ZoneCreatePage from './pages/ZonesPage/ZoneCreatePage/ZoneCreatePage';
 import ZoneEditPage from './pages/ZonesPage/ZoneEditPage/ZoneEditPage';
 import DevicesListPage from './pages/DevicesPage/DevicesListPage';
-import DeviceCreatePage from './pages/DevicesPage/DeviceCreatePage' // Nouveau
+import DeviceCreatePage from './pages/DevicesPage/DeviceCreatePage';
+import DeviceDetailsPage from './pages/DevicesPage/DeviceDetailsPage';
 import AnalyticsPage from './pages/AnalyticsPage/AnalyticsPage';
 import SettingsPage from './pages/SettingsPage/SettingsPage';
-import { GoogleMapsProvider } from './context/GoogleMapsContext';
 
 const theme = createTheme({
   palette: {
@@ -25,24 +25,26 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <GoogleMapsProvider>
-        <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Navigate to="/dashboard" replace />} />
-              <Route path="dashboard" element={<DashboardPage />} />
-              <Route path="zones" element={<ZonesListPage />} />
-              <Route path="zones/create" element={<ZoneCreatePage />} />
-              <Route path="zones/edit/:id" element={<ZoneEditPage />} />
-              <Route path="devices" element={<DevicesListPage />} />
-              <Route path="devices/create" element={<DeviceCreatePage />} /> // Ajoutez ceci
-              <Route path="analytics" element={<AnalyticsPage />} />
-              <Route path="settings" element={<SettingsPage />} />
-            </Route>
-          </Routes>
-        </Router>
-      </GoogleMapsProvider>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          <Route path="dashboard" element={<DashboardPage />} />
+          
+          {/* Routes Zones */}
+          <Route path="zones" element={<ZonesListPage />} />
+          <Route path="zones/create" element={<ZoneCreatePage />} />
+          <Route path="zones/edit/:id" element={<ZoneEditPage />} />
+          
+          {/* Routes Devices - ORDRE IMPORTANT */}
+          <Route path="devices/create" element={<DeviceCreatePage />} />
+          <Route path="devices/:id" element={<DeviceDetailsPage />} />
+          <Route path="devices" element={<DevicesListPage />} />
+          
+          <Route path="analytics" element={<AnalyticsPage />} />
+          <Route path="settings" element={<SettingsPage />} />
+        </Route>
+      </Routes>
     </ThemeProvider>
   );
 }
--- web-admin/src/main.jsx
+++ web-admin/src/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { AuthProvider } from './context/AuthContext';
+import { GoogleMapsProvider } from './context/GoogleMapsContext';
 import App from './App';
 import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <AuthProvider>
+          <GoogleMapsProvider>
+            <App />
+          </GoogleMapsProvider>
+        </AuthProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
